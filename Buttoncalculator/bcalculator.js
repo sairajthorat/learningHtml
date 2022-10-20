bfb55dd94b93cf3032a81dfdf4ae889b15fbb143
sairajthorat <thoratsairaj10@gmail.com>
2022-10-20 21:44:46 +0530

    calulator(error solving in progress)

---
+++ Buttoncalculator/bcalculator.js
@@ -0,0 +1,52 @@
+let string = "";
+let a = document.querySelectorAll('.num');
+Array.from(a).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
+
+
+let b = document.querySelectorAll('.sign');
+Array.from(b).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
+
+
+
+let c = document.querySelectorAll('.op');
+Array.from(c).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'AC'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
